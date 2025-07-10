e15d1dd664708e54a0e3f8b9d4bbcd8a66244ba3
curuzzang <129712232+curuzzang@users.noreply.github.com>
2025-07-10 15:52:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ import pytz
 import streamlit as st
 
 # ✅ 현재 시간 (KST)
-korea = pytz.timezone("Asia/Seoul")
-now = datetime.now(korea)
+#korea = pytz.timezone("Asia/Seoul")
+#now = datetime.now(korea)
 
 # ✅ 마감 시각: 2025년 7월 2일 밤 11시 59분 59초
-cutoff_datetime = korea.localize(datetime(2025, 7, 2, 23, 59, 59))
+#cutoff_datetime = korea.localize(datetime(2025, 7, 2, 23, 59, 59))
 
 # ✅ 제한 조건
-if now > cutoff_datetime:
-    st.error("⛔ 앱 사용시간이 종료되었습니다! 감사합니다💕")
-    st.stop()
+#if now > cutoff_datetime:
+#    st.error("⛔ 앱 사용시간이 종료되었습니다! 감사합니다💕")
+#    st.stop()
 
 # 초기 설정
 st.set_page_config(page_title="나의 그림상자 (Assistant API)", layout="wide")
